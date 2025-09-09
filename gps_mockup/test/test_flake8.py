527178112d5d2cf25565da69ce7a282cb467d3ad
Adam Wróblewski <adam01wroblewski@gmail.com>
2025-09-09 20:44:55 +0200

    add mockup - forgotten files

---
+++ gps_mockup/test/test_flake8.py
@@ -0,0 +1,33 @@
+# Copyright 2024 Your Name
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import os
+import unittest
+
+from ament_flake8 import get_file_names
+from ament_flake8 import main
+
+
+class TestFlake8(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        cls.path = os.path.dirname(__file__)
+
+    def test_flake8(self):
+        main(['--exclude', 'test', '--exclude', 'build', '--exclude', 'install', '--exclude', 'log', '.'])
+
+
+if __name__ == '__main__':
+    unittest.main()
